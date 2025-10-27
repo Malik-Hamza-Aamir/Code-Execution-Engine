f53e5a3ada38fb37c297afb8163ef1029a323a0d
Hamza Aamir <hamzaaamir6@gmail.com>
2025-10-27 21:30:37 +0500

    web seperated

---
+++ apps/web/src/app.tsx
@@ -0,0 +1,23 @@
+import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+export function App(props: any) {
+    return (
+        <>
+            <RouterProvider router={props.router} />
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop
+                closeOnClick
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
+        </>
+    );
+}
+
+export default App;
